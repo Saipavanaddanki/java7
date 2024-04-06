570a7c4b2414d15b23d25993489a244fdded2f49
sai <addanki.saipavan@gmail.com>
2024-04-06 15:27:50 +0530

    Maven_Example03 projectleader 1st time

---
+++ Maven_Example03/src/main/java/com/nit/service/App.java
@@ -0,0 +1,21 @@
+package com.nit.service;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       int i=20;
+       int j=30;
+    }
+    
+    public static int add(int i,int j) {
+    	return i+j;
+    }
+    public static int sub(int i,int j) {
+    	return i-j;
+    }
+    public int mul(int i,int j) {
+    	return i*j;
+    }
+    
+}
---
+++ Maven_Example03/src/test/java/com/Test/Test2.java
@@ -0,0 +1,53 @@
+package com.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import com.nit.service.App;
+
+public class Test2 {
+
+	private static App app;
+	@BeforeAll
+	public static void setUp() {
+		 app=new App();
+	}
+
+	@Test
+	@Order(2)
+	//@DisplayName("")
+	public void testWithPositiveOnAdd() {
+		int i=10;
+		int j=20;
+		int add=i+j;
+		assertEquals(App.add(i, j),add);
+		
+	}
+	@Test
+	//@DisplayName("")
+	@Order(5)
+	public void testWithPositiveOnSub() {
+		int i=10;
+		int j=20;
+		int sub=i-j;
+		assertEquals(sub,app.sub(i, j));
+	}
+	@Test
+	//@DisplayName("")
+	@Order(8)
+	public void testWithPositiveOnMul() {
+		int i=10;
+		int j=20;
+		int mul=i*j;
+		assertEquals(mul,app.mul(i, j));
+	}
+	@AfterAll
+	public static void tearDown() {
+		app=null;
+	}
+}
